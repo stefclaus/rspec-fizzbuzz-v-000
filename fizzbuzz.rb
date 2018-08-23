5fd9624adace8667975355825bb6cca721d81e59
Stef Claus <stef.claus@gmail.com>
2018-08-23 01:01:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 && int % 5 == 0 
+       "FizzBuzz"
    elsif int % 5 == 0 
      "Buzz"
-     else int % 3 == 0 && int % 5 == 0 
-       "FizzBuzz"
+     else int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
     end
   end
  
